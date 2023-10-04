f5d45dc48f7b1401378e7a1c2f96d29814e67a3a
Matt March <matt.marchcoding@gmail.com>
2023-10-04 11:22:34 -0400

    updating file

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
@@ -17,95 +23,18 @@ const db = knex({
   db.select('*').from('users').then(data => {console.log(data)});
 
 
-const app = express();
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-app.use(cors())
+  const app = express();
 
-app.get('/',(req, res) =>{
-    res.send('success')
-});
 
-app.post('/signin', (req,res) => {
-
-    db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
-    .then(data =>{
-       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-        if(isValid){
-            return db.select('*').from('users')
-              .where('email', '=', req.body.email)
-              .then(user =>{
-                res.json(user[0])
-              })
-              .catch(err => res.status(400).json('unable to get user'))
-        }else {
-            res.status(400).json('wrong credentials')
-        }
-
-    })
-    .catch(err => res.status(400).json('wrong credentials'))
-})
-
-app.post('/register', (req, res) => {
-    const {email, name, password} = req.body;
-    // bcrypt.hash(password, null, null, function (err, hash) {
-    //     console.log(hash);
-    // });
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email,
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-           return trx('users')
-           .returning('*')
-           .insert({
-                email: loginEmail[0].email,
-                name: name,
-                joined: new Date()
-            })
-            .then(user => {
-                res.json(user[0])
-            })
-
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-
-    .catch(err => res.status(400).json("unable to register"))
-
-})
-
-    app.get('/profile/:id', (req,res) => {
-        const { id } = req.params;
-        db.select('*').from('users').where({id})
-        .then(user => {
-            if(user.length){
-           res.json(user[0]);
-            }else {
-                res.status(400).json('Not found')
-            }
-        })
-        .catch(err => res.status(400).json('error getting user'))
-
-    });
-
-    app.put('/image', (req,res) => {
-        const { id } = req.body;
-        db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries =>{
-            res.json(entries[0].entries)
-        })
-        .catch(err => res.status(400).json('unable to get entries'))
-
-    });
+  app.use(cors())
+  app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+  
+  app.get('/', (req, res)=> { res.send(db.users) })
+  app.post('/signin', signin.handleSignin(db, bcrypt))
+  app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+  app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+  app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+  app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
     app.listen(process.env.PORT || 3000, ()=> {
         console.log(`app is running on port ${process.env.PORT}`)
