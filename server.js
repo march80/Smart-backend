1d78002b19646764fe831fa1be37b36ae235e19e
Matt March <matt.marchcoding@gmail.com>
2023-10-04 11:38:14 -0400

    removing first get

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const db = knex({
   app.use(cors())
   app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
   
-  app.get('/', (req, res)=> { res.send(db.users) })
+  //app.get('/', (req, res)=> { res.send(db.users) })
   app.post('/signin', signin.handleSignin(db, bcrypt))
   app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
   app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
