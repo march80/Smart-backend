d0f635a89103b523f0be6e807823cc62641703c9
Matt March <matt.marchcoding@gmail.com>
2023-10-04 14:10:54 -0400

    adding db

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const db = knex({
   
   app.get('/', (req, res)=> { res.send(db.users) })
   app.post('/signin', signin.handleSignin(db, bcrypt))
-  app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+  app.post('/register', (req, res) => {console.log(req + " " + res)}//{ register.handleRegister(req, res, db, bcrypt) })
   app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
   app.put('/image', (req, res) => { image.handleImage(req, res, db)})
   app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
